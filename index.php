c0408a24e877a17a4ac735ea3146dd95f3ecc4a7
tianrosandhy <tianrosandhy@gmail.com>
2017-11-02 14:36:07 +0800

    Penerapan di ChartJS

--- index.php
+++ index.php
@@ -29,7 +29,18 @@ $y = [
 
 
 $regresi = new RegresiLinier($x, $y);
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Latihan Regresi Linier</title>
+    <script src="Chart.min.js"></script>    
+</head>
+<body>
+
+<?php
 //contoh menampilkan data regresi linear
 echo "<textarea style='width:100%; height:300px; '>";
 echo "X Variable : \n";
@@ -45,3 +56,37 @@ echo "<textarea style='width:100%; height:300px; '>";
 echo "Hasil Analisis Regresi Linear Sederhana : \n";
 print_r($regresi->all);
 echo "</textarea>";
+?>
+<br>
+<h3>Penerapan RegresiLinearPHP dengan ChartJS</h3>
+
+<canvas id="graph" width=500 height="150"></canvas>
+<script>
+    ctx = document.getElementById('graph');
+    var chart = new Chart(ctx, {
+        type : 'line',
+        data: {
+            labels: [<?=implode(",",$x)?>],
+            datasets: [
+                {
+                label: 'Realisasi Kehadiran',
+                data: [<?=implode(",", $y)?>],
+                backgroundColor: 'rgba(12, 199, 132, 0.2)',
+                borderWidth: 1
+                },
+                {
+                label: 'Regresi Linier',
+                data: [<?=implode(",", $regresi->all)?>],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderWidth: 1
+                },
+            ]
+        }
+    });
+</script>
+
+
+</body>
+</html>
+
+
